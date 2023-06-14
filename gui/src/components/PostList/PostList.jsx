2da754197e90b44740d08790f24ff42cc5c5fc79
jackitaliano <italiano.16@osu.edu>
2023-06-14 18:00:29 -0500

    changed questionlist to postlist and added postcard

---
+++ gui/src/components/PostList/PostList.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function PostList(props) {
+    return (props.postsList.map((post) => {
+        return (
+            <div>
+
+            </div>
+        )
+    })
+    )
+}
--- gui/src/components/QuestionList/QuestionList.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function QuestionList() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
