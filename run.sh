c7c730ced9ddff4c164e61e8dd048913bf9d2d14
Chaseforyourlife <chasefashing@gmail.com>
2023-06-14 12:30:13 -0500

    Fix run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+conda init bash
 conda env update -f eso-env.yml
-cmd.exe /k "call cd gui & call npm update & call echo Starting Frontend - Run 'npm start'"
+#cmd.exe /k "call cd gui & call npm update & call echo Starting Frontend - Run 'npm start'"
 #eval "$(conda shell.cmd.exe hook)"
 conda activate eso-env
 python run_api.py
