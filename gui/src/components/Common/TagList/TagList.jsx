28baf020b665413a547a9cc78452e3e707787d76
jackitaliano <italiano.16@osu.edu>
2023-06-14 12:54:42 -0500

    added default rcc snippets

---
+++ gui/src/components/Common/TagList/TagList.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TagList extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default TagList;
