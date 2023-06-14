af4844ba332f2c2fefe0f0540011f58850db4b01
zambangeloK2 <104877921+zambangeloK2@users.noreply.github.com>
2023-06-14 18:13:22 -0500

    navbar

--- gui/src/components/NavBar/NavBar.jsx
+++ gui/src/components/NavBar/NavBar.jsx
@@ -5,12 +5,23 @@ import './NavBar.css'
 export default function NavBar() {
   return (
     <nav className = "nav">
-      <a href="/Home">Home</a>
+      <a href="/Home" className='site-title'>
+        Home
+      </a>
       <ul>
-        <li>
-          <a href="/Profile">Profile</a>
-        </li>
+        <CustomLink href='/profile'>Profile</CustomLink>
       </ul>
     </nav>
+  )
+}
+
+function CustomLink({ href, children, ...props }) {
+  const path = window.location.pathname
+  return (
+    <li className={path === href ? "active" : ""}>
+      <a href={href} {...props}>
+        {children}
+      </a>
+    </li>
   )
 }
