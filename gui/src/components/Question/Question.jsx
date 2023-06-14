c60f0d4bbada2b2fd7d577691e0204850aa6d0b9
Chaseforyourlife <chasefashing@gmail.com>
2023-06-14 13:38:01 -0500

    Stop Session

--- gui/src/components/Question/Question.jsx
+++ gui/src/components/Question/Question.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import './Question.css'
 
-export default function Question() {
+export default function Question(title, content, tagList, userInfo, timeStamp, commentList, votes) {
   return (
-    <div>Question</div>
+    <div>
+      <div id="title-bar">
+        <label>{title}</label>
+        <div>
+          <TimeStamp time={timeStamp}/>
+          <UserTag info={userInfo}/>
+        </div>
+
+        <Content text={content}/>
+      </div>
+    </div>
   )
 }
---
+++ gui/src/components/SortQuestionsBar/SortQuestionsBar.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import './SortQuestionsBar'
+
+function handleSortClick(sortType) {
+    // call helper with based on sort type
+}
+
+export default function SortQuestionsBar() {
+  return (
+    <div>
+        <button 
+        id="interesting-button"
+        onClick={()=> handleSortClick("interesting")}
+        >Interesting</button>
+
+        <button 
+        id="hot-button"
+        onClick={()=> handleSortClick("hot")}
+        >Hot</button>
+
+        <button 
+        id="week-button"
+        onClick={()=> handleSortClick("week")}
+        >Week</button>
+
+        <button 
+        id="month-button"
+        onClick={()=> handleSortClick("month")}
+        >Month</button>
+    </div>
+  )
+}
