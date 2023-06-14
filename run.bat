be6404f9672b50c9df7cd37f6523af1ffe03d7b4
Chaseforyourlife <chasefashing@gmail.com>
2023-06-14 12:25:21 -0500

    add run bash script

--- run.bat
+++ run.bat
@@ -1,4 +1,5 @@
-start cmd.exe /k "call cd gui & call echo Starting Frontend - Run 'npm start'"
+conda env update -f eso-env.yml
+start cmd.exe /k "call cd gui & call npm update & call echo Starting Frontend - Run 'npm start'"
 call conda init cmd.exe
 call conda activate eso-env
 call python run_api.py
