2663cbda7a6552a760560c6aa5a644b5f7eff9ee
Chaseforyourlife <chasefashing@gmail.com>
2023-06-14 12:08:50 -0500

    Example Finished

--- api/_main/routes_main.py
+++ api/_main/routes_main.py
@@ -1,13 +1,17 @@
 from flask import Blueprint,make_response
 from .functions import *
+from api.db.mysql_functions import read_to_df
+#from api.db.mysql_functions import *
+import pandas as pd
 
 blue_main = Blueprint('blue_main',__name__)
 
 @blue_main.route('/test_route')
 def test_route():
-
-    return make_response({
-        'data':['Chase','Leo','Kenny','Lydia', 'Angelo','Cameron','Jack','Randy']
+    test_df:pd.DataFrame = read_to_df("SELECT * FROM test_table")
+    test_data_list = test_df["value"].to_list()
+    return make_response({  
+        'data':test_data_list
     })
 
 
