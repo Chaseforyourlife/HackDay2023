a1ab5fd959f2f4e8fa67090c860f0c10b2736e3d
Chaseforyourlife <chasefashing@gmail.com>
2023-06-14 12:28:02 -0500

    Edit run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 conda env update -f eso-env.yml
 cmd.exe /k "call cd gui & call npm update & call echo Starting Frontend - Run 'npm start'"
-eval "$(conda shell.cmd.exe hook)"
+#eval "$(conda shell.cmd.exe hook)"
 conda activate eso-env
 python run_api.py
 read -p "Press any key to continue..."
