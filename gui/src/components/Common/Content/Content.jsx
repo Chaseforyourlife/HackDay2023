9ccba6b507d5cd45c7c920e633f651ce31be9e5d
jackitaliano <italiano.16@osu.edu>
2023-06-14 12:53:52 -0500

    added folders

---
+++ gui/src/components/Common/Content/Content.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Content extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Content;
