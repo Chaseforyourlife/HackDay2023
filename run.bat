e4e1562f38eb17df5812ba45e1668a2257ee6ea4
Chaseforyourlife <chasefashing@gmail.com>
2023-06-14 12:47:59 -0500

    add call to run.bat

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
-conda env update -f eso-env.yml
+call conda env update -f eso-env.yml
 start cmd.exe /k "call cd gui & call npm update & call echo Starting Frontend - Run 'npm start'"
 call conda init cmd.exe
 call conda activate eso-env
