445a4cfa8de316fbcc6a0533bd4d9a2e53399039
Chaseforyourlife <chasefashing@gmail.com>
2023-06-14 21:53:58 -0500

    Co-authored-by: LydiaBloomfield <LydiaBloomfield@users.noreply.github.com>

--- gui/src/components/QuestionCard/QuestionCard.jsx
+++ gui/src/components/QuestionCard/QuestionCard.jsx
@@ -1,25 +1,43 @@
 import React from 'react';
 import { Box, Card, CardContent, CardActions, Typography, Button } from '@mui/material';
 import TagList from '../Common/TagList/TagList'
+import "./QuestionCard.css"
+import {
+    MDBCard,
+    MDBCardHeader,
+    MDBCardBody,
+    MDBCardTitle,
+    MDBCardText,
+    MDBBtn
+} from 'mdb-react-ui-kit';
 
 const QuestionInfo = (props) => {
     console.log(JSON.stringify(props))
     console.log(props.questionInfo)
     return (
         <Box>
-            <Card variant="outlined">
+            <Card variant="outlined" classname="card">
                 <CardContent>
                     <Box>
-                        <Typography>
+                        <Typography style={{ fontSize: }}>
                             {props.questionInfo.title}
                         </Typography>
-                        <TagList tagList={props.questionInfo.tagList}/>
+                        <TagList tagList={props.questionInfo.tagList} />
                     </Box>
                 </CardContent>
                 <CardActions>
-                    <Button variant="contained" sx={{background: "red"}}>View Question</Button>
+                    <Button variant="contained" sx={{ background: "red" }}>View Question</Button>
                 </CardActions>
             </Card>
+
+            {/* <div class="card">
+                <h5 class="card-header">Featured</h5>
+                <div class="card-body">
+                    <h5 class="card-title">Special title treatment</h5>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div> */}
         </Box>
     );
 }
--- gui/src/components/QuestionList/QuestionList.jsx
+++ gui/src/components/QuestionList/QuestionList.jsx
@@ -3,6 +3,10 @@ import QuestionCard from '../QuestionCard/QuestionCard'
 
 
 export default function QuestionList(props) {
-    return (props.questions.map((question) => <QuestionCard questionInfo={question}/>)
-    )
+    return (props.questions.map((question) => 
+    <>
+        <QuestionCard questionInfo={question}/>
+        <br/>
+    </>
+    ))
 }
